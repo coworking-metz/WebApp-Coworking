a6d7c84056f1c1fe11879438cb9f667be48b5dbe
Matthieu Petit <p.matthieu@me.com>
2023-07-03 15:07:17 +0200

    feat(auth): check user access sbefore each route

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-export default createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: () => import('../Home.vue'),
-    },
-    {
-      path: '/login',
-      component: () => import('../Login.vue'),
-    },
-    {
-      path: '/admin/portail',
-      component: () => import('../vues/Admin/Portail.vue'),
-    },
-  ],
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: () => import('../Home.vue'),
+        },
+        {
+            path: '/login',
+            component: () => import('../Login.vue'),
+            meta: {
+                isPublic: true,
+            },
+        },
+        {
+            path: '/admin/portail',
+            component: () => import('../vues/Admin/Portail.vue'),
+        },
+    ],
 });
+
+router.beforeEach((to, _from, next) => {
+    if (!to.matched.some((route) => route.meta.isPublic)) {
+        return next();
+    }
+
+    // TODO: should check with API if user is logged in and/or access rights
+    // return http
+    //     .fetchAccessToken()
+    //     .then(() => next())
+    //     .catch(() => {
+    //         // should redirect to login page
+    //         // and show an error message
+    //         next({
+    //             name: '/login',
+    //             query: { ...to.query, redirect: to.path },
+    //         });
+    //     });
+});
+
+export default router;
